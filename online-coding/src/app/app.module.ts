6d354a3e42c892f8419f158364f51fc255a64180
Kornel Toth <tkornel19@gmail.com>
2023-05-07 12:46:25 +0000

    topbar

--- online-coding/src/app/app.module.ts
+++ online-coding/src/app/app.module.ts
@@ -5,15 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './pages/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
+    MatFormFieldModule,
     BrowserAnimationsModule
   ],
   providers: [],
